8fa94d0f834465d58cd4b6d563d9e4617a8259c5
AbhijithTyss <abhijith.styss@gmail.com>
2024-11-15 09:24:09 +0530

    Added class in github package

---
+++ Selenium_Automation_8/src/github/ToLearnGitHub.java
@@ -0,0 +1,11 @@
+package github;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class ToLearnGitHub {
+	@Test
+	public void git() {
+		Reporter.log("Added class in github package");
+	}
+}
